1c59368367ba52eba86fb1de5a98f329928732e8
xczq <290353142@qq.com>
2020-12-02 15:17:29 +0800

    from kjsoft 2020-04-13

--- js/tool.js
+++ js/tool.js
@@ -1,18 +1,15 @@
 window.Tool=(function(){
-    console.log("加载完成");
-    var cmd = {
-        copy:["屏蔽复制拦截",()=>window.addEventListener("copy",e=>e.stopImmediatePropagation(),true)]
-    }
+    console.log("Tool 加载完成，代码示例：");
+    console.log("屏蔽复制拦截:",getcmdhelp("copy"));
 
-    
-    function getcmdhelp(name){return `fetch("//cnxzq.github.io/js/tool.js").then(d=>d.text()).then(t=>eval(t),Tool.${name}())`}
-    function help(){
-        console.log("代码示例");
-        for(var a in cmd){
-            console.log(cmd[a],":",getcmdhelp(a));
-        }
+    return {
+        copy
+    };
+
+    function getcmdhelp(name){return `fetch("//cnxzq.github.io/js/tool.js").then(d=>d.text()).then(t=>eval(t)).then(_=>Tool.${name}())`}
+
+    function copy(){
+        window.addEventListener("copy",e=>e.stopImmediatePropagation(),true);
+        console.log("屏蔽成功");
     }
-    var rev = {help};
-    for(var a in cmd){ rev[a]=cmd[a][1]; }
-    return rev;
 })()
